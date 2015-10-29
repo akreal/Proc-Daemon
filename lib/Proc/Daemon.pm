18dbded1bcf7882e9b806c44d56efb0134090ad5
Pavel Denisov <pavel.a.denisov@gmail.com>
2015-10-28 20:23:48 -0400

    Update version

--- lib/Proc/Daemon.pm
+++ lib/Proc/Daemon.pm
@@ -22,7 +22,7 @@ package Proc::Daemon;
 use strict;
 use POSIX();
 
-$Proc::Daemon::VERSION = '0.21';
+$Proc::Daemon::VERSION = '0.22';
 
 
 ################################################################################
